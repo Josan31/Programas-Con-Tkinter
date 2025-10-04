f568dceb5567c9b8eac8f628d41f8ea50afebd2b
José Manuel Sánchez <josemsanchezg511@gmail.com>
2025-10-04 17:31:56 -0500

    ejercicios

--- Registro_De_Usuario.py
+++ Registro_De_Usuario.py
@@ -6,12 +6,3 @@ ventana.geometry("400x300")
 
 ventana.mainloop()
 
-
-
-#fatal: No configured push destination.
-#Either specify the URL from the command-line or configure a remote repository using
-
-#   git remote add <name> <url>
-
-#and then push using the remote name
-#    git push <name>
