0d6fae65f5a69be177378ee0a4f1c1feddf1e083
José Manuel Sánchez <josemsanchezg511@gmail.com>
2025-10-04 17:22:23 -0500

    Tkinter

--- Registro_De_Usuario.py
+++ Registro_De_Usuario.py
@@ -4,3 +4,14 @@ ventana = tk.Tk()
 ventana.title("Registro de Usuario")
 ventana.geometry("400x300")
 
+ventana.mainloop()
+
+
+
+#fatal: No configured push destination.
+#Either specify the URL from the command-line or configure a remote repository using
+
+#   git remote add <name> <url>
+
+#and then push using the remote name
+#    git push <name>
