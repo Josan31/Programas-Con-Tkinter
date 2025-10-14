aa1711e2e0a075d59a956b00e4d809122efb02fd
José Manuel Sánchez <josemsanchezg511@gmail.com>
2025-10-14 15:46:34 -0500

    Commit

--- TKINTER/Suma_Numeros.py
+++ TKINTER/Suma_Numeros.py
@@ -1,17 +1,61 @@
-import tkinter as tk
+import tkinter as tk    # 'as tk' es una convención que hace el código más llimpio y fácil de leer
 
-# ------ FUNCIONES ------
+# ---------- FUNCIONES ----------
 
 def sumar():
     try:
-        numero1 = float(entrada1.get())
-        numero2 = float(entrada2.get())
+        # Obtiene el valor de las entradas 
+        numero1 = int(entrada1.get())
+        numero2 = int(entrada2.get())
+        
+        # Realiza la suma entre los dos números
         resultado.set (numero1 + numero2)
+   
+    # Si la enytrada no es un número
     except ValueError:
+
         resultado.set("Error")
 
-# ------ VENTANA PRINCIPAL ------
+# ---------- VENTANA PRINCIPAL ----------
 
+# Se crea la ventana principal
 ventana = tk.Tk()
-ventana.tittle ("Calculadora básica")
 
+# Se le asigna un título a la ventana
+ventana.title("Calculadora básica")
+
+# Se le asigna el alto y ancho de la ventana
+ventana.geometry("250x180")    # Tamaño
+
+# ---------- VARIABLE ----------
+
+resultado = tk.StringVar()
+
+# ---------- WIDGETS ----------
+
+# Etiqueta para el número 1
+tk.Label(ventana, text="Número 1: ").pack(pady=5)
+
+# Campo de entrada para el primer número
+entrada1 = tk.Entry(ventana)
+entrada1.pack()
+
+# Etiqueta para el número 2
+tk.Label(ventana, text="Número 2: ").pack(pady=5)
+
+# Campo de entrada para el segundo número
+entrada2 = tk.Entry(ventana)
+entrada2.pack()
+
+# Botón para sumar que llama a la función 'sumar'
+tk.Button(ventana, text="Sumar", command=sumar).pack(pady=5)
+
+tk.Label(ventana, text="Resultado: ").pack
+
+# Etiqueta para mostrar el resultado de color azul y características específicas
+tk.Label(ventana, textvariable=resultado, fg="blue", font=("Arial", 12)).pack()
+
+# ---------- LOOP PRINCIPAL ----------
+
+# Bucle que mantiene la ventana en constante ejecución
+ventana.mainloop()
